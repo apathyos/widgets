c5c195a2fe1ddb8f93d6870ce54b408ad93bddaf
apathoid <apathoid@tuta.io>
2026-08-03 00:12:20 +0300

    features: fix music player subscriptions

--- lib/features/MusicPlayer/MusicPlayer.tsx
+++ lib/features/MusicPlayer/MusicPlayer.tsx
@@ -7,9 +7,9 @@ import { AlbumCoverSize } from '../../types/albumCover';
 import { SPACING_M, SPACING_S } from '../../constants/widget';
 import { Classes } from '../../types/utils';
 import cn from 'classnames';
-import { updateAccessor } from '../../utils/misc';
-
-const MprisService = MprisModule.get_default();
+import { unpackAccessor, updateAccessor } from '../../utils/misc';
+import { execution } from '@apathoid/utils';
+import { Delay } from '../../types/common';
 
 export interface IMusicPlayer {
     classes?: Classes<'root'>;
@@ -18,54 +18,71 @@ export interface IMusicPlayer {
 export function MusicPlayer(props: IMusicPlayer) {
     const { classes } = props;
 
-    const mpris = new Mpris(MprisService);
-    let player = mpris.getPlayer();
+    const mpris = new Mpris();
+    const player = mpris.player;
 
-    const isPlayerAvailable = createBinding(player, 'trackid').as(v => !!v);
-    const length = createBinding(player, 'length');
-    const position = createBinding(player, 'position');
-    const playbackStatus = createBinding(player, 'playbackStatus');
-    const isPaused = createComputed(get => !get(isPlayerAvailable) || get(playbackStatus) !== MprisModule.PlaybackStatus.PLAYING);
-    const coverArt = createBinding(player, 'coverArt');
-    const title = createBinding(player, 'title');
-    const album = createBinding(player, 'album');
-    const artist = createBinding(player, 'artist');
+    const onPlay = () => unpackAccessor(player)?.play_pause();
+    const onPrev = () => unpackAccessor(player)?.previous();
+    const onNext = () => unpackAccessor(player)?.next();
 
-    MprisService.connect('notify', () => (player = mpris.getPlayer()));
-
-    const onPlay = () => player.play_pause();
-    const onPrev = () => player.previous();
-    const onNext = () => player.next();
+    const seekWithDebounce = execution.asyncDebounce(async (value: number) => {
+        return unpackAccessor(player)?.set_position(value);
+    }, Delay.M);
 
     return (
-        <box
-            orientation={Gtk.Orientation.VERTICAL}
-            hexpand
-            class={updateAccessor(classes?.root, root => cn(root, 'music-player'))}
-        >
-            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={SPACING_M} visible={isPlayerAvailable((v) => !!v)}>
-                <AlbumCover file={coverArt} size={AlbumCoverSize.M} />
-                <box orientation={Gtk.Orientation.VERTICAL} spacing={SPACING_S}>
-                    <SongMetaLabel label={title} />
-                    <With value={album}>{(album) => album && <SongMetaLabel label={album} />}</With>
-                    <SongMetaLabel label={artist} />
-                </box>
-            </box>
+        <With value={player}>
+            {player => {
+                if (!player) {
+                    return <PlayerControls isPaused />;
+                }
 
-            <PlayerControls
-                isPaused={isPaused}
-                onPlay={onPlay}
-                onPrev={onPrev}
-                onNext={onNext}
-            />
-            <box visible={isPlayerAvailable((v) => !!v)} hexpand>
-                <PlayerSeeker
-                    min={0}
-                    max={length}
-                    value={position}
-                    onChange={({ event }) => player.set_position(event.value)}
-                />
-            </box>
-        </box>
+                const isPlayerAvailable = createBinding(player, 'trackid').as(v => !!v);
+                const length = createBinding(player, 'length');
+                const position = createBinding(player, 'position');
+                const playbackStatus = createBinding(player, 'playbackStatus');
+                const isPaused = createComputed(get => {
+                    return !get(isPlayerAvailable) || get(playbackStatus) !== MprisModule.PlaybackStatus.PLAYING;
+                });
+                const coverArt = createBinding(player, 'coverArt');
+                const title = createBinding(player, 'title');
+                const album = createBinding(player, 'album');
+                const artist = createBinding(player, 'artist');
+
+                return (
+                    <box
+                        class={updateAccessor(classes?.root, root => cn(root, 'music-player'))}
+                        orientation={Gtk.Orientation.VERTICAL}
+                        hexpand
+                    >
+                        <box visible={isPlayerAvailable} orientation={Gtk.Orientation.HORIZONTAL} spacing={SPACING_M}>
+                            <AlbumCover file={coverArt} size={AlbumCoverSize.M} />
+                            <box orientation={Gtk.Orientation.VERTICAL} spacing={SPACING_S}>
+                                <SongMetaLabel label={title} />
+                                <With value={album}>
+                                    {album => album && <SongMetaLabel label={album} />}
+                                </With>
+                                <SongMetaLabel label={artist} />
+                            </box>
+                        </box>
+
+                        <PlayerControls
+                            isPaused={isPaused}
+                            onPlay={onPlay}
+                            onPrev={onPrev}
+                            onNext={onNext}
+                        />
+
+                        <box visible={isPlayerAvailable} hexpand>
+                            <PlayerSeeker
+                                min={0}
+                                max={length}
+                                value={position}
+                                onChange={({ event }) => seekWithDebounce(event.value)}
+                            />
+                        </box>
+                    </box>
+                );
+            }}
+        </With>
     );
 }
--- lib/models/Mpris/Mpris.ts
+++ lib/models/Mpris/Mpris.ts
@@ -1,11 +1,43 @@
 import MprisModule from 'gi://AstalMpris?version=0.1';
+import { Accessor, createBinding, createExternal } from 'gnim';
+import { unpackAccessor } from '../../utils/misc';
+import { Delay } from '@/types/common';
 
 export class Mpris {
-    constructor(private MprisService: MprisModule.Mpris) {}
+    private readonly mpris = MprisModule.get_default();
 
-    getPlayer(args?: { name?: string }): MprisModule.Player {
-        const { name = 'playerctld' } = args ?? {};
+    readonly busName = 'org.mpris.MediaPlayer2.playerctld';
+    readonly player: Accessor<MprisModule.Player | null>;
 
-        return MprisModule.Player.new(name);
+    constructor() {
+        this.player = createExternal<MprisModule.Player | null>(null, (setPlayer) => {
+            const players = createBinding(this.mpris, 'players');
+
+            const update = (players: MprisModule.Player[]) => {
+                if (!players.length) {
+                    setPlayer(null);
+                }
+
+                if (players.length && !unpackAccessor(this.player)) {
+                    setPlayer(this.getDefaultPlayer());
+                }
+            };
+
+            const timer = setTimeout(() => update(this.mpris.players));
+
+            const playersSub = players.subscribe(() => {
+                clearTimeout(timer);
+                update(unpackAccessor(players));
+            });
+
+            return () => {
+                playersSub();
+                setPlayer(null);
+            };
+        });
+    }
+
+    private getDefaultPlayer() {
+        return MprisModule.Player.new(this.busName);
     }
 }
